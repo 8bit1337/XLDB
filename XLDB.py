4413c670b73fc1cc10df18daad01d2d628cad2c9
8bit1337 <8bit1337@gmail.com>
2016-01-02 11:21:42 -0500

    20160102-1121
    Not much really - next goals are to figure out how to digitally record
    file names so that the content matters, not the file name

--- XLDB.py
+++ XLDB.py
@@ -1,7 +1,3 @@
-#from os import listdir
-#from os.path import isfile, join
-
-
 #onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 #or you could use os.walk() which will yield two lists for each directory it visits - splitting into files and dirs for you. If you only want the top directory you can just break the first time it yields
 
@@ -13,8 +9,20 @@
   #  break
 
 from os import listdir
-#chdir('C:/Users/jasong.MARTRON/Games/Atari800WinPLus/Games')
-files = listdir('C:/Users/jasong.MARTRON/Games/Atari800WinPLus/Games')
-print(files[0])
-
+from os.path import isfile, join
+currdir = 'C:/Users/Jason/Games/Atari800WinPLus/Games'
+#currdir = 'C:/Users/jasong.MARTRON/Games/Atari800WinPLus/Games'
+files = listdir(currdir)
+i=0
+print(files[0:3])
+for f in files:
+    if isfile(join(currdir, f)):
+        print('File: ' + f)
+    else:
+        print('Dir : ' + f)
+    
+    i+=1
+    if i == 3:
+        break
+    
 
