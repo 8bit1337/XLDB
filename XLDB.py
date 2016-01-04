687c8f7e79df3be09e1a6caa86dab11faefef1c5
8bit1337 <8bit1337@gmail.com>
2016-01-04 17:15:55 -0500

    20160104-1715
    This is working code, but the data it produces isn't unique.  I will
    attempt MD5SUMS next - I initially balked at the idea because of the
    time it takes, but it looks inevitable and it looks to be guaranteed
    unique

--- XLDB.py
+++ XLDB.py
@@ -1,42 +1,40 @@
-#onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#or you could use os.walk() which will yield two lists for each directory it visits - splitting into files and dirs for you. If you only want the top directory you can just break the first time it yields
-
-#from os import walk
-
-#f = []
-#for (dirpath, dirnames, filenames) in walk(mypath):
- #   f.extend(filenames)
-  #  break
-
+##### INITIALIZE #####
 from os import listdir
 from os.path import isfile, join, dirname
 workdir = dirname(__file__)
 ataridir = 'C:/Users/Jason/Games/Atari800WinPLus/Games'
 #ataridir = 'C:/Users/jasong.MARTRON/Games/Atari800WinPLus/Games'
 
-files = listdir(ataridir)
-i=0
+### FUNCTIONS ###
+def processDir(files, directory, exportfile):
+    dirs = []
 
-data = open(join(workdir, 'DataDump.csv'), 'w')
+    print('Processing: ' + directory)
+    for f in files:
+        currf = join(directory, f)
+        if isfile(currf):
+            file = open(currf, 'rb')
+            first71 = str(file.read(71)).replace('|','').replace('\\','')
 
-
-dirs = []
-for f in files:
-    currf = join(ataridir, f)
-    if isfile(currf):
-        file = open(currf, 'rb')
-        first71 = str(file.read(71))
+            towrite = '|'.join([f, first71]) + '\n'
+               
+            exportfile.write(towrite)
+        else:
+            dirs += [f]
         
-        towrite = '|'.join([f, first71])
-        print(towrite)
-        data.write(towrite)
-    i+=1
-    if i == 3:
-        break
 
-for di in dirs:
-    print('Dir : ' + d)
+    #now process sub directories
+    for d in dirs:
+        newdir = directory + '/' + d
+        files = listdir(newdir)
+        processDir(files, newdir, exportfile)
 
-file.close()
-data.close()
+##### KERNAL ####
+exportfile = open(join(workdir, 'DataDump.csv'), 'w')
+files = listdir(ataridir)
+processDir(files, ataridir, exportfile)
+
+
+exportfile.close()
+
 
