6b83405aac8016c17e9ad555315d0b351408747a
8bit1337 <8bit1337@gmail.com>
2017-01-27 09:14:03 -0500

    20170127-0913

---
+++ XLDB.sql
@@ -0,0 +1,8 @@
+--select * from (
+--select First71, SUM(1) as Count from DataDump group by First71
+--) t
+--order by Count desc
+
+select * from DataDump 
+where First71='b''x96x02x80x16x80x00x00x00x00x00x00x00x00x00x00x00x00x03x00x07@x15Lx14x07x03x03x00x1ax01x04x00}xcbx07xacx0ex07xf06xadx12x07x85Cx8dx04x03xadx13x07x85Dx8dx05x03xadx10x07xacx0fx07x18xaex0ex07 lx070'''
+order by [FileName]
